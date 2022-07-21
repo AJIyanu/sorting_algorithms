296e2397df2271078ca84c71b62ddd3b5f7ca5f1
AJIyanu <aderemijoseph@live.com>
2022-07-21 20:06:50 +0100

    hope no errors

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,79 @@
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * swaplist - swaps list with previous
+ * @add: address of swapping
+ * @head: incase we need to become the head
+ *
+ * Return: no shit
+ */
+
+void swaplist(listint_t *add, listint_t **head)
+{
+	listint_t *node = add;
+	listint_t *temprev = node->prev;
+	listint_t *tempnext = node->next;
+
+	if (temprev->prev == NULL)
+	{
+		node->prev->next = node->next;
+		node->next->prev = node->prev;
+		node->prev->prev = node;
+		node->next = node->prev;
+		node->prev = NULL;
+		*head = node;
+
+	}
+	else
+	{
+		temprev->prev->next = node;
+		node->prev = temprev->prev;
+		node->next = temprev;
+		temprev->next = tempnext;
+		temprev->prev = node;
+		if (tempnext != NULL)
+			tempnext->prev = temprev;
+	}
+}
+
+/**
+ * insertion_sort_list - sorts linked list by insertion method
+ * @list: head addrress of list
+ *
+ * Return: no shit
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *wemove = *list;
+	listint_t *swapadd = NULL;
+	int swap = 0;
+
+	while (wemove->next != NULL)
+	{
+		if (wemove->n > wemove->next->n)
+		{
+			swaplist(wemove->next, list);
+			print_list(*list);
+			swap = 1;
+			swapadd = wemove->prev;
+		}
+		if (swap == 0)
+			wemove = wemove->next;
+		while (swap == 1)
+		{
+			if (swapadd->prev == NULL)
+				break;
+			if (swapadd->n < swapadd->prev->n)
+			{
+				swaplist(swapadd, list);
+				print_list(*list);
+			}
+			else
+			{
+				swap = 0;
+			}
+		}
+	}
+}
