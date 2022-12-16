e90b7e87416117e011bbc4329401e6d744173096
AJIyanu <aderemijoseph@live.com>
2022-12-16 20:32:56 +0100

    updated

--- sort.h
+++ sort.h
@@ -1,5 +1,9 @@
-#ifndef __SORT_H_
-#define __SORT_H_
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -7,16 +11,22 @@
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
+
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
-void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void swapper(listint_t *a, listint_t *b);
+void swaps(int *array, size_t size, int *a, int *b);
+void kwiksort(int *array, size_t size, ssize_t low, ssize_t high);
+size_t partition(int *array, size_t size, ssize_t low, ssize_t high);
 #endif
